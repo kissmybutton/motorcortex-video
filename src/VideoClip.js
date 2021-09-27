8ad0dc2529a478b9ab1cf28b566268bca2ad2a87
Andreas Trantidis <atrantidis@gmail.com>
2021-09-27 22:17:24 +0300

    refactor(dependencies): moved from kissmybutton to donkeyclip

--- src/VideoClip.js
+++ src/VideoClip.js
@@ -1,4 +1,4 @@
-import { BrowserClip } from "@kissmybutton/motorcortex";
+import { BrowserClip } from "@donkeyclip/motorcortex";
 export default class VideoClip extends BrowserClip {
   get html() {
     this.width = this.attrs.width || 640;
